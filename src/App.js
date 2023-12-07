6e0e32b7838624ffbaf69aae0eb09774b9c0d8cd
sushmita09 <sushmita09@tamu.edu>
2023-12-07 01:30:53 -0600

    draft5

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
 import Papa from 'papaparse';
 
 import TableauEmbed from './components/TableauEmbed';
 
 
 
+
 const App = () => {
 
-  const [selectedVariables, setSelectedVariables] = useState({ first: 'GDP', second: 'Personal_Income'});
+  const [selectedVariables, setSelectedVariables] = useState({ first: 'Depression_Percentage', second: 'Number_Of_Internet_Users'});
   
   const handleVariableChange = (e, position) => {
     if (position === 'first'){
@@ -44,6 +45,8 @@ const App = () => {
       Depression_Percentage: 19.6,
       Happiness_Index: 7.12,
       Inflation: 0.10,
+      Number_Of_Internet_Users: 280000000,
+      
     },
     {
       name: '2016',
@@ -54,6 +57,7 @@ const App = () => {
       Depression_Percentage: 20.1,
       Happiness_Index: 7.10,
       Inflation: 1.30,
+      Number_Of_Internet_Users: 282100000
     },
     {
     name: '2017',
@@ -64,6 +68,8 @@ const App = () => {
     Depression_Percentage: 20.7,
     Happiness_Index: 6.99,
     Inflation: 2.10,
+    Number_Of_Internet_Users: 286900000,
+    OTT_Subscribers: 153000000,
   },
   {
     name: '2018',
@@ -74,6 +80,8 @@ const App = () => {
     Depression_Percentage: 22.0,
     Happiness_Index: 6.89,
     Inflation: 2.40,
+    Number_Of_Internet_Users: 286900000,
+    OTT_Subscribers: 170100000,
   },
   {
     name: '2019',
@@ -84,6 +92,8 @@ const App = () => {
     Depression_Percentage: 21.6,
     Happiness_Index: 6.89,
     Inflation: 1.80,
+    Number_Of_Internet_Users: 312300000,
+    OTT_Subscribers: 181500000,
 
   },
   {
@@ -95,6 +105,8 @@ const App = () => {
     Depression_Percentage: 22.9,
     Happiness_Index: 6.95,
     Inflation: 1.20,
+    Number_Of_Internet_Users: 288100000,
+    OTT_Subscribers: 188300000,
   },
   {
     name: '2021',
@@ -105,6 +117,8 @@ const App = () => {
     Depression_Percentage: 28.6,
     Happiness_Index: 6.98,
     Inflation: 4.70,
+    Number_Of_Internet_Users: 298800000,
+    OTT_Subscribers: 193200000,
   },
   {
     name: '2022',
@@ -115,6 +129,8 @@ const App = () => {
     Depression_Percentage: 26.9,
     Happiness_Index: 6.89,
     Inflation: 8.00,
+    Number_Of_Internet_Users: 370200000,
+    OTT_Subscribers: 197700000,
   },
   ];
 
@@ -131,6 +147,8 @@ const App = () => {
     fetchData();
   }, []);
 
+  
+
 
   return (
     
@@ -144,9 +162,7 @@ const App = () => {
 
       
       <div className="chart-container">
-      <p style={{ fontFamily: "'Times New Roman', Times, serif", padding: '50px', fontSize:30, color: 'rgb(158, 236, 239)', textAlign:'left'}}>
-          Exploring data trends across various socioeconomic factors from 2015 to 2022 to understand the impact of the COVID-19 pandemic on key indicators like GDP, mental health, employment, and more. 
-        </p>
+      
         <div className="dropdown-container" >
         
         <select className="btn btn-lg btn-outline-info text-light dropdown-toggle"
@@ -161,8 +177,9 @@ const App = () => {
           <option value="Depression_Percentage">Depression Percentage</option>
           <option value="Happiness_Index">Happiness Index</option>
           <option value="Inflation">Inflation</option>
+          <option value="Number_Of_Internet_Users">Internet Users</option>
+          <option value="OTT_Subscribers">OTT Subscribers</option>
           
-        
 
         </select>
       
@@ -177,14 +194,17 @@ const App = () => {
           <option value="Personal_Income">Personal Income</option>
           <option value="Depression_Percentage">Depression Percentage</option>
           <option value="Happiness_Index">Happiness Index</option>
-          <option value="Inflation">Inflation</option> 
+          <option value="Inflation">Inflation</option>
+          <option value="Number_Of_Internet_Users">Internet Users</option>
+          <option value="OTT_Subscribers">OTT Subscribers</option>
+
         </select> 
         </div>
       
         <div className="line-chart">
          <LineChart
           width={1000}
-          height={500}
+          height={800}
           data={data01}  
           margin={{ top: 100, right: 100, left: 100, bottom: 100 }}
           
@@ -193,29 +213,35 @@ const App = () => {
           <XAxis 
             dataKey="name"
             label={{ value: "Year", fontSize: 24, fill: 'white', dy:50, }} 
-            tick={{ fontSize: 22, fill: 'white' }} 
+            tick={{ fontSize: 22, fill: 'white', dy:10 }} 
           />
           <YAxis 
             yAxisId="left" 
             label={{ value: selectedVariables.first, angle: -90, position: 'insideLeft', fontSize: 24, fill: '#8884d8', offset:-30, textAnchor:'middle', dy:100, }}
             tickFormatter={(value) => formatNumber(value)} 
             tick={{ fontSize: 22, fill: 'white' }}  
+            domain={['auto', 'auto']}
           />
           <YAxis 
             yAxisId="right" 
             label={{ value: selectedVariables.second, angle: +90, position: 'insideRight', fontSize: 24, fill: '#82ca9d', offset:-30, textAnchor:'middle', dy:100 }}
             tickFormatter={(value) => formatNumber(value)}
             tick={{ fontSize: 22, fill: 'white' }} 
+            domain={['auto', 'auto']} 
             orientation="right" />
           <Tooltip 
             formatter={(value, name) => [formatNumber(value), name]}
-            contentStyle={{backgroundColor: 'transparent', color: 'white', border:'none', textAlign: 'left', fontSize:'24px'}} 
-            />
+            contentStyle={{backgroundColor: 'transparent', color: 'white', border:'none', textAlign: 'left', fontSize:'24px'}}  
+          />
           
           <Line yAxisId="left" type="monotone" dataKey={selectedVariables.first} stroke="#8884d8" strokeWidth={3} activeDot={{ r: 8 }} />
           <Line yAxisId="right" type="monotone" dataKey={selectedVariables.second} stroke="#82ca9d" strokeWidth={3} activeDot={{ r: 8 }}/>
           </LineChart>
           </div>
+
+          <p style={{ fontFamily: "'Times New Roman', Times, serif", padding: '50px', fontSize:25, color: 'rgb(158, 236, 239)', textAlign:'left'}}>
+          Exploring data trends across various socioeconomic factors from 2015 to 2022 to understand the impact of the COVID-19 pandemic on key indicators like GDP, mental health, employment, and more. 
+        </p>
       </div>
 
       <div>
--- src/components/TableauEmbed.js
+++ src/components/TableauEmbed.js
@@ -1,4 +1,5 @@
 import React, { useRef, useEffect } from "react";
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
 
 const { tableau } = window;
@@ -9,16 +10,39 @@ function TableauEmbed() {
     const ref3 = useRef(null);
     const ref4 = useRef(null);
     const ref5 = useRef(null);
+    
 
+    //economic
     const url5 = "https://public.tableau.com/views/unemp_2_17017078712220/Dashboard1?:toolbar=no&:showShareOptions=false";
     const url2 = "https://public.tableau.com/shared/Y7WNJ9BMW";
-    const url3 = "https://public.tableau.com/shared/7T89RR9G4";
+
+    //happiness 
+    const url3 = "https://public.tableau.com/views/happiness_choropleth/Dashboard2";
+    
+    
+    //ott
     const url4 = "https://public.tableau.com/views/OTTsubscriptiontrendsyear/Dashboard1";
     const url1 = "https://public.tableau.com/views/Industry_GDP/Dashboard1";
 
     const options = {
         device: "desktop",
+        width: '1000px',
+        height: '800px'
     };
+    const options_2 = {
+        device: "desktop",
+        width: '400px',
+        height: '400px' 
+    }
+
+    const ott_data = [
+    
+        { year: 2018, Netflix: 147.4, Amazon_Video: 93.3, Hulu: 57, ESPN: 3.4 },
+        { year: 2019, Netflix: 154.4, Amazon_Video: 106.6, Hulu: 76.3, ESPN: 12.6 },
+        { year: 2020, Netflix: 168.9, Amazon_Video: 130.1, Hulu: 94.5, ESPN: 3.4 },
+        { year: 2021, Netflix: 171.7, Amazon_Video: 136.9, Hulu: 99.7, ESPN: 3.4 },
+        { year: 2022, Netflix: 175.5, Amazon_Video: 143.2, Hulu: 106.1, ESPN: 3.4 },
+      ];
 
     useEffect(() => {
         let viz1 = null;
@@ -26,6 +50,7 @@ function TableauEmbed() {
         let viz3 = null;
         let viz4 = null;
         let viz5 = null;
+        
 
         const initViz = () => {
             // Ensure that the previous viz is cleaned up before creating a new one
@@ -47,15 +72,20 @@ function TableauEmbed() {
             if (viz5) {
                 viz5.dispose();
             }
+           
 
             viz1 = new tableau.Viz(ref1.current, url1, options);
             viz2 = new tableau.Viz(ref2.current, url2, options);
             viz3 = new tableau.Viz(ref3.current, url3, options);
             viz4 = new tableau.Viz(ref4.current, url4, options);
             viz5 = new tableau.Viz(ref5.current, url5, options);
+           
+
         };
 
         initViz(); // Call initViz once when the component mounts
+
+        
 
         return () => {
             //Cleanup when the component unmounts
@@ -78,31 +108,95 @@ function TableauEmbed() {
             if (viz5) {
                 viz5.dispose();
             }
+
+            
         };
-    }, [url1, url2, url3, url4, url5, options]);
+    }, [url1, url2, url3, url4, url5, options, ott_data]);
 
     return (
         <div>
             <p className="section-heading">Economic Trends</p>
-            <div className="tableau-container" ref={ref1}></div>
+            <div className="chart-container">
+            <p style={{ fontFamily: "'Times New Roman', Times, serif", fontSize:25, color: 'rgb(158, 236, 239)', textAlign:'left'}}>
+                This analysis dissects the impact on various industries, revealing the percentage contributions of each sector to the overall GDP landscape.
+            </p>
+            <div className="tableau-container" ref={ref1}> </div>
+            </div>
             <br/>
-
+            <div className="chart-container">
+            <p style={{ fontFamily: "'Times New Roman', Times, serif", fontSize:25, color: 'rgb(158, 236, 239)', textAlign:'left'}}>
+            This choropleth dynamically illustrates the fluctuation of state-level unemployment rates from 2013 to 2023, offering users the flexibility to select specific years or play through the entire decade for a comprehensive view of the evolving employment landscape.
+            </p>
             <div className="tableau-container" ref={ref5}></div>
+            </div>
 
             <p className="section-heading">Mental Health Insights</p>
+            <div className="chart-container">
             <div className="tableau-container" ref={ref3}>
+            </div>
+            <p style={{ fontFamily: "'Times New Roman', Times, serif", fontSize:25, color: 'rgb(158, 236, 239)', textAlign:'left'}}>
+            Explore the changing happiness landscape in the United States with this choropleth map, comparing the top 10 happiest and bottom 10 happiest states in the pre-pandemic year 2018 to the post-pandemic year 2022.
+            </p>
             </div>
             <br/>
             <br/>
 
-            <img src='lifetime depression.png'
-                style={{ width: '100%', maxWidth: '800px', margin: '10px auto' }}
+           <div className="chart-container"> 
+            <img src='depression_rate.png'
+                style={{ width: '100%', maxWidth: '800px', margin: '10px auto', border:'2px solid #000' , borderRadius: '8px'}}
             />
-            
-            
+            <p className="custom-paragraph">
+            Recent data from a Gallup Panel survey (Feb. 21-28, 2023) reveals concerning trends in depression diagnoses among U.S. adults. 
+            <br/><br/>
+            Over one-third of women (36.7%) report a lifetime diagnosis, compared to 20.4% of men, with women's rates rising nearly twice as fast since 2017. 
+            <br/><br/>
+            Those aged 18 to 29 (34.3%) and 30 to 44 (34.9%) show significantly higher lifetime diagnosis rates than older age groups. 
+            <br/><br/>
+            Current depression or treatment rates are highest among women (23.8%) and adults aged 18 to 29 (24.6%), with notable increases since 2017. 
+            <br/><br/>
+            The post-COVID rise is attributed to factors like social isolation, job losses, and healthcare worker stress. </p>
+            </div>
 
+
+            
             <p className="section-heading">OTT Insights</p>
+            <div className="chart-container">
+                <p className="custom-paragraph">This bar chart visually captures the watchtime trends of OTT platforms in the United States from 2018 to 2022, providing a clear snapshot of the evolving user preferences over the specified time period.</p>
             <div className="tableau-container" ref={ref4}></div>
+            </div>
+            <br/>
+            <div className="line-chart">
+            <LineChart
+                width={1000}
+                height={900}
+                data={ott_data}  
+                margin={{ top: 100, right: 100, left: 100, bottom: 100 }}
+                
+                >
+                <CartesianGrid strokeDasharray="3 3" stroke="#383323"/>
+                <XAxis 
+                    dataKey="year"
+                    label={{ value: "Year", fontSize: 24, fill: 'white', dy:50, offset: -30 }} 
+                    tick={{ fontSize: 22, fill: 'white', dy:10 }} 
+                />
+                <YAxis
+                    label={{ value: "Number of Viewers in Millions", angle: -90, position: 'insideLeft', fontSize: 24, fill: 'white', offset:-30, textAnchor:'middle', dy:120, }}
+                    tick={{ fontSize: 22, fill: 'white' }}  
+                    
+                />
+                <Tooltip 
+                formatter={(value, name) => [value, name]}
+                contentStyle={{backgroundColor: 'transparent', color: 'white', border:'none', textAlign: 'left', fontSize:'24px'}}      
+                />
+                <Legend verticalAlign="top" height={36} />
+
+                <Line type="monotone" dataKey="Netflix" stroke="#8884d8" strokeWidth={3} activeDot={{ r: 8 }}/>
+                <Line type="monotone" dataKey="Amazon_Video" stroke="#82ca9d" strokeWidth={3} activeDot={{ r: 8 }} />
+                <Line type="monotone" dataKey="Hulu" stroke="pink" strokeWidth={3} activeDot={{ r: 8 }} />
+                <Line type="monotone" dataKey="ESPN" stroke="yellow" strokeWidth={3} activeDot={{ r: 8 }} />
+                
+            </LineChart>
+            </div>
             <br/>
             <div className="tableau-container" ref={ref2}></div>
         </div>
